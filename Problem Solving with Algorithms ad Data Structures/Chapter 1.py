679c52a9a4d80be86991553a3ac576b3014eb763
zstall <zstall4@gmail.com>
2018-08-07 16:19:45 -0600

    Reading Text, adding notes and examples ch1

---
+++ Problem Solving with Algorithms ad Data Structures/Chapter 1.py
@@ -0,0 +1,57 @@
+#! Python3
+
+
+
+'''
+****************************************************************
+Chapter 1: Introducation
+
+1.1: Objectives
+1.2: Getting Started
+1.3: What is Computer Science?
+1.4: What is Programming?
+1.5: Why Study Data Structures and Abstract Data Types?
+
+notes: An ABSTRACT DATA TYPE, or ADT, is a logical description of
+how we view data and the operations that are allowed without regard
+to how they will be implemented. We only concern ourselves with what
+the data represents and not how it will be constructed. By providing
+this level of abstraction, we are ENCAPSULATING the data. This is
+so we can focus on the complexity of the task at hand, and not get
+lost in the details.
+
+1.6: Why Study Algorithms?
+1.7: Review of Basic Python
+1.8: Getting started with Python
+
+notes: A CLASS (In all OBJECT ORIENTED PROGRAMMING/OOP i.e. python or
+java...) is the description of wha the data looks like (the state) and
+what the data can do (the behavior).
+Classes are analogous to abstract data types because a user of a class
+only sees the state and behavior of a data item. Data items are called
+OBJECTS in the OOP paradigm. An OBJECT is an instance of a class.
+
+EX) Python classes:
+    int -- Integer
+    float -- Decimal
+    bool -- True/False
+    List -- []
+    strings
+    tuples
+    dictoinary
+
+1.9: Input and Output
+
+notes: Often we will need to interact with the user, python provides us
+with a easy method to ask for information from the user. The function is
+called the INPUT function.
+
+EX) aName = input("Please enter your name: ")
+
+    print( "Your name in all capitals is", aName.upper(),
+           "and has length", len(aname)) 
+
+
+
+****************************************************************
+'''
